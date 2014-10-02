ca93b14d724cdafb3b2d9fe07e99dc9d925d430d
Roman Tereshin <tereshin.roman@gmail.com>
2014-10-02 18:12:07 +0400

    open recursion and framework-based solution comparison was maded in test110.ml

--- regression/test110.ml
+++ regression/test110.ml
@@ -43,3 +43,54 @@ class ['e, 'inh] arith_eval_transformer = object inherit ['e, 'inh, int, 'inh, i
   method c_Add inh _ left right = left.fx inh + right.fx inh
   method c_Mul inh _ left right = left.fx inh * right.fx inh
 end
+
+
+module ByHand = struct
+
+module StringSet = Set.Make (String)
+
+let lam_fold self acc = function
+  | Var _ -> acc
+  | App (func, arg) -> self (self acc func) arg
+  | Lam (_, body) -> self acc body
+
+let open_vars self acc = function
+  | Var var_name -> StringSet.add var_name acc
+  | other -> lam_fold self acc other
+
+let vars : lam -> StringSet.t =
+  let rec vars_acc acc term = open_vars vars_acc acc term in
+  vars_acc StringSet.empty
+
+let open_free_vars self acc = function
+  | Lam (var_name, body) ->
+      let lam_free_vars = StringSet.remove var_name (self StringSet.empty body) in
+      StringSet.union acc lam_free_vars
+  | other -> open_vars self acc other
+
+let free_vars : lam -> StringSet.t =
+  let rec free_vars_acc acc term = open_free_vars free_vars_acc acc term in
+  free_vars_acc StringSet.empty
+
+end
+
+let test_term =
+  Lam ("x",
+       App (Lam ("y",
+                 Var "y"),
+            Var "z"))
+
+let string_set_of_list xs = List.fold_right StringSet.add xs StringSet.empty
+
+let my_assert : bool -> unit = fun assertion ->
+  if not assertion then
+    print_endline "[FAIL]"
+
+let vars_expected = ["y"; "z"]
+let free_vars_expected = ["z"]
+
+let () =
+  my_assert (StringSet.elements (vars test_term) = vars_expected);
+  my_assert (StringSet.elements (free_vars test_term) = free_vars_expected);
+  my_assert (StringSet.elements (ByHand.vars test_term) = vars_expected);
+  my_assert (StringSet.elements (ByHand.free_vars test_term) = free_vars_expected);
